6fa48f2d9cda91e9af27de8cedd8acfba9980dd8
Meka <antoine.hoarau@inria.fr>
2013-07-22 05:15:28 -0700

    Recompiled core

--- src/apps/m3humanoid_shm_test-ens/m3humanoid_shm_test.cpp
+++ src/apps/m3humanoid_shm_test-ens/m3humanoid_shm_test.cpp
@@ -83,7 +83,7 @@ void StepHumanoidShm(int cntr)
 	cmd.right_arm.q_desired[i] = 0.0;
 	cmd.right_arm.tq_desired[i] = 40.0;
 	cmd.right_arm.slew_rate_q_desired[i] = 20.0;
-	cmd.right_arm.q_stiffness[i] = 0.0;	
+	cmd.right_arm.q_stiffness[i] = 1.0;	
       }
       
 
